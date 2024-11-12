b757d6504cc033020258a2272009ec95fd92b03c
Adrian Hassa <adrianhassa@gmail.com>
2024-11-12 13:31:54 +0100

    #29 Move unit selection to the first tab

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ config = load_config()
 root = tk.Tk()
 root.title(config.get("title", "Generate Requirements"))
 
+# Get the values from the config files
 ip_var=tk.StringVar(value="localhost:11435")
 path_var=tk.StringVar(value=config['folder_path'])
 if "latency_paragraphs.csv" in config['latency_paragraphs']:
@@ -33,19 +34,17 @@ output_var=tk.StringVar(value=config['latency_paragraphs'])
 keyword_var=tk.StringVar(value=' '.join(config['keywords']).replace(" ", ","))
 model_var=config['model_name']
 
+# Create a tabbed interface
 tabControl = ttk.Notebook(root)
 tab1 = ttk.Frame(tabControl)
 tabControl.add(tab1, text='Generate Requirements')
 tab2 = ttk.Frame(tabControl)
 tabControl.add(tab2, text='Download Standards')
-tab3 = ttk.Frame(tabControl)
-tabControl.add(tab3, text='Settings')
 tabControl.pack(expand=1, fill="both")
 
+# Function to save the configuration to the JSON file
 def save_config():
-
     checked_units = [unit for unit, var in unit_vars.items() if var.get()]
-
     config = {
         'llm_address': ip_var.get(),
         'folder_path': path_var.get(),
@@ -68,6 +67,7 @@ def save_config():
         json.dump(config, f)
     return config
 
+# Function to update the status label and progress bar
 def update_status(message, progress=None):
     status_label.config(text=message)
     if progress is not None:
@@ -77,17 +77,19 @@ def update_status(message, progress=None):
         progressbar.grid_forget()
     root.update()
 
+# Function to start the filtering on button press
 def run():
     run_btn.config(state=tk.DISABLED)
     update_status("Starting the filtering...")
     config = save_config()
     try:
+        # Function to run on a separate thread
         def run_tasks():
             filter_docs.execute_filtering(config, update_status)
             generate_req.generate_req(config, update_status)
             csv2xlsx.csv_to_xlsx(config, update_status)
             run_btn.config(state=tk.NORMAL)
-        
+        # Start the thread
         task_thread = threading.Thread(target=run_tasks)
         task_thread.start()
     except Exception as e:
@@ -109,7 +111,7 @@ def select_output_dir():
 
 # Set window size
 width = config.get("width", 700)
-height = config.get("height", 350)
+height = config.get("height", 400)
 root.geometry(f"{width}x{height}")
 
 ip_label = tk.Label(tab1, text = 'LLM address:port', font=('arial',10))
@@ -130,11 +132,25 @@ model_label = tk.Label(tab1, text = 'Language Model', font = ('arial',10,'normal
 model_entry=ttk.Combobox(tab1, values=["llama3.1", "llama3.1:70b", "llama3.1:405b"], width=79)
 model_entry.set(model_var)
 
+unit_label = tk.Label(tab1, text = 'Units', font = ('arial',10,'normal'))
+unit_frame = tk.Frame(tab1)
+
+unit_vars = {}  # Dictionary to store BooleanVar for each unit
+# Create a checkbox for each unit type in the config
+col = 0
+for unit_name in config['units']:
+    unit_var = tk.BooleanVar(value=unit_name in config.get('checked_units', []))
+    unit_vars[unit_name] = unit_var
+    unit_checkbox = tk.Checkbutton(unit_frame, text=unit_name, variable=unit_var)
+    unit_checkbox.grid(row=0, column=col, padx=25)
+    col += 1
+
 run_btn=tk.Button(tab1,text = 'Run', command = run, width=30)
 
 status_label = tk.Label(tab1, text = '', font = ('arial',10,'normal'))
 progressbar = ttk.Progressbar(tab1, length=500)
 
+# Place the widgets on the window grid
 ip_label.grid(row=0,column=0, padx=5)
 ip_entry.grid(row=0,column=1, pady=10)
 model_label.grid(row=1,column=0, padx=5)
@@ -147,20 +163,25 @@ output_entry.grid(row=3,column=1, pady=10)
 output_btn.grid(row=3,column=2, pady=10)
 keyword_label.grid(row=4,column=0, padx=5)
 keyword_entry.grid(row=4,column=1, pady=10)
+unit_label.grid(row=5, column=0, padx=5)
+unit_frame.grid(row=5, column=1, pady=10)
 
-run_btn.grid(row=5,column=1)
-status_label.grid(row=6,column=1)
+run_btn.grid(row=6,column=1)
+status_label.grid(row=7,column=1)
 
+# Callback function to update the download status
 def update_download_status(message):
     status2_label.config(text=message)
     root.update()
 
+# Function to start the download on button press
 def download():
     status2_label.config(text="Processing the standards...")
     root.update()
     config = save_config()
     threading.Thread(target=std_retriever.download, args=(config, download_btn, update_download_status)).start()
     
+# Directory selection dialog
 def select_download_dir():
     download_dir_var = filedialog.askdirectory()
     download_dir_entry.delete(0, tk.END)
@@ -183,15 +204,7 @@ download_dir_btn=tk.Button(tab2,text = '...', command = select_download_dir, wid
 download_btn=tk.Button(tab2, text = 'Download', command = download, width=30)
 status2_label = tk.Label(tab2, text = '', font = ('arial',10,'normal'))
 
-unit_vars = {}  # Dictionary to store BooleanVar for each unit
-
-# Create a checkbox for each unit type in the config
-for unit_name in config['units']:
-    unit_var = tk.BooleanVar(value=unit_name in config.get('checked_units', []))
-    unit_vars[unit_name] = unit_var
-    unit_checkbox = tk.Checkbutton(tab3, text=unit_name, variable=unit_var)
-    unit_checkbox.grid(sticky='w')
-
+# Place the widgets on the window grid
 series_label.grid(row=0,column=0, padx=5)
 series_entry.grid(row=0,column=1, pady=10)
 search_keyword_label.grid(row=1,column=0, padx=5)
